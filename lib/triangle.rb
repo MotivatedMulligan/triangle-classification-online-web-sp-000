a77c1c5029719c5d69dd7e8a7e9800581e2ab564
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-05-02 19:02:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,18 +6,25 @@ class Triangle
   # end
 
 
-  def initialize a, b, c
-    @sides = [a, b, c]
-    @a, @b, @c = [a,b,c].sort
-    raise TriangleError unless valid_triangle?
-    @type = triangle_type
+  def initialize (a, b, c)
+    @a = a
+    @b = b 
+    @c = c
+      
+    # rescue ExceptionName
+    #   
+    # end
+    # @sides = [a, b, c]
+    # @a, @b, @c = [a,b,c].sort
+    # raise TriangleError unless valid_triangle?
+    # @type = triangle_type
     #attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
 #+++++
   def triangle(a, b, c)
     sorted_triangle_lengths = [a, b, c].sort
-  #a, b, c = [a, b, c].sort
+
   raise TriangleError if (sorted_triangle_lengths[0] + sorted_triangle_lengths[1] <= sorted_triangle_lengths)
   raise TriangleError if (a <= 0 || b <= 0 || c <= 0)
   return :equilateral if (a==b && b==c && a == c)
@@ -25,16 +32,15 @@ class Triangle
   return :scalene
 end
 
-  def valid_triangle?
-    !!(@a > 0 && @a + @b > @c)
-  end
-  def triangle_type
-    [:equilateral, :isosceles, :scalene][[@a, @b, @c].uniq.size -1]
-  end
+  # def valid_triangle?
+  #   !!(@a > 0 && @a + @b > @c)
+  # end
+  # def triangle_type
+  #   [:equilateral, :isosceles, :scalene][[@a, @b, @c].uniq.size -1]
+  # end
 
    class TriangleError < StandardError
   end
 
 end
-class TriangleError < StandardError
-end
+
