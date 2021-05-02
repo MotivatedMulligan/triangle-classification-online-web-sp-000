37e4377719d09c613df7b8a38760179192a3ba8b
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-05-02 18:35:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+   attr_accessor :a, :b, :c
+  #
+  # def initialize(attributes)
+  #   attributes.each {|key, value| self.send(("#{key}="), value)}
+  # end
+
+  
+  def initialize a, b, c
+    @sides = [a, b, c]
+    @a, @b, @c = [a,b,c].sort
+    raise TriangleError unless valid_triangle?
+    @type = triangle_type
+    #attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
+#+++++
+  def triangle(a, b, c)
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0 or a + b <= c
+  return :equilateral if a == c
+  return :isosceles if a == b or b == c
+  return :scalene
+end
+
+  def valid_triangle?
+    !!(@a > 0 && @a + @b > @c)
+  end
+  def triangle_type
+    [:equilateral, :isosceles, :scalene][[@a, @b, @c].uniq.size -1]
+  end
+
+   class TriangleError < StandardError
+  end
+
+end
+class TriangleError < StandardError
 end
