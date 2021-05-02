3c0412deeb9d80e7cff2937bf220b348b68baa15
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-05-02 19:14:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
   #  sorted_triangle_lengths = [a, b, c].sort
   # raise TriangleError if (sorted_triangle_lengths[0] + sorted_triangle_lengths[1] <= sorted_triangle_lengths)
   raise TriangleError if (a <= 0 || b <= 0 || c <= 0)
-  return :equilateral if (a == b && b == c && a == c)
+  return :equilateral if (a == b == c )
   return :isosceles if (a == b) || (a == c) || (b == c)
   return :scalene
 end
